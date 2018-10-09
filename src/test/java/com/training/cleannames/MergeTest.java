473b54932a95d9b334833079d6f66ef58cdb3fe9
firastaiem <6563700+firastaiem@users.noreply.github.com>
2018-10-08 23:36:13 -0400

    Initial commit

---
+++ src/test/java/com/training/cleannames/MergeTest.java
@@ -0,0 +1,94 @@
+package com.training.cleannames;
+
+import org.apache.commons.io.FileUtils;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static org.testng.Assert.*;
+
+public class MergeTest {
+    private Merge merge;
+
+    @BeforeMethod
+    public void setup() {
+        File destDirectory = new File("./testing/dest");
+        if (destDirectory.exists()) {
+            try {
+                FileUtils.deleteDirectory(destDirectory);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        this.merge = new Merge();
+    }
+
+    @Test
+    public void shouldCopyFilesWhenSourceDirectoryHasFiles() {
+        File sourceDir = new File("./testing/test_source");
+        sourceDir.mkdirs();
+        Date date = new Date();
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        int year  = localDate.getYear();
+        int month = localDate.getMonthValue();
+        int day   = localDate.getDayOfMonth();
+
+        String destDirectory = year + "-" + month + "-" + day;
+        File destFile = new File("./testing/dest/"+destDirectory);
+
+        File file1 = new File(sourceDir.getPath() + "/file1.txt");
+        try {
+            FileUtils.writeStringToFile(file1,"File 1");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File file2 = new File(sourceDir.getPath() + "/file2.txt");
+        try {
+            FileUtils.writeStringToFile(file2,"File 2");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        merge.copy(new File("./testing/test_source"),true);
+        assertEquals(2,destFile.listFiles().length);
+
+
+    }
+
+    @Test
+    public void shouldCopyFilesWhenSourceDirectoryHasDuplicateFiles() {
+        File sourceDir = new File("./testing/test_source");
+        sourceDir.mkdirs();
+        Date date = new Date();
+        LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+        int year  = localDate.getYear();
+        int month = localDate.getMonthValue();
+        int day   = localDate.getDayOfMonth();
+
+        String destDirectory = year + "-" + month + "-" + day;
+        File destFile = new File("./testing/dest/"+destDirectory);
+
+
+        File file1 = new File(sourceDir.getPath() + "/file1.txt");
+        try {
+            FileUtils.writeStringToFile(file1,"File 1");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        File file2 = new File(sourceDir.getPath() + "/file2.txt");
+        try {
+            FileUtils.writeStringToFile(file2,"File 2");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        merge.copy(new File("./testing/test_source"),true);
+        merge.copy(new File("./testing/test_source"),true);
+        assertEquals(4,destFile.listFiles().length);
+
+    }
+}
